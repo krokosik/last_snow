a19724e47c26b9ab68c56d07e1c4f5ef1a1b9259
krokosik <wikkra@gmail.com>
2024-01-06 15:39:07 +0100

    Basic OSC server

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{fs, path::PathBuf, env};
+use rosc::{OscPacket};
+use std::net::UdpSocket;
+use std::thread;
+use std::{env, fs, path::PathBuf};
 
 use tauri_plugin_log::LogTarget;
 
@@ -38,22 +41,26 @@ fn get_new_filename(base_dir: &PathBuf) -> PathBuf {
     fs::create_dir_all(base_dir.join("sentences")).unwrap();
 
     // List all csv files in the directory
-    fs::read_dir(base_dir.join("sentences")).unwrap().for_each(|entry| {
-        let entry = entry.unwrap();
-        last_csv_idx = std::cmp::max(
-            entry
-                .path()
-                .file_stem()
-                .unwrap()
-                .to_str()
-                .unwrap()
-                .parse::<usize>()
-                .unwrap(),
-            last_csv_idx,
-        );
-    });
+    fs::read_dir(base_dir.join("sentences"))
+        .unwrap()
+        .for_each(|entry| {
+            let entry = entry.unwrap();
+            last_csv_idx = std::cmp::max(
+                entry
+                    .path()
+                    .file_stem()
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+                    .parse::<usize>()
+                    .unwrap(),
+                last_csv_idx,
+            );
+        });
 
-    base_dir.join("sentences").join(format!("{}.csv", last_csv_idx + 1))
+    base_dir
+        .join("sentences")
+        .join(format!("{}.csv", last_csv_idx + 1))
 }
 
 fn write_sentence(row: &Row, file_path: &PathBuf, headers: bool) {
@@ -63,26 +70,20 @@ fn write_sentence(row: &Row, file_path: &PathBuf, headers: bool) {
         .create(true)
         .open(file_path)
         .unwrap();
-    
+
     let mut wtr = csv::Writer::from_writer(file);
-    
+
     if headers {
         wtr.serialize(row).unwrap();
     } else {
-        wtr.write_record(&[
-            &row.language,
-            &row.sentence,
-            &row.timestamp,
-        ]).unwrap();
+        wtr.write_record(&[&row.language, &row.sentence, &row.timestamp])
+            .unwrap();
     }
     wtr.flush().unwrap();
 }
 
 #[tauri::command]
-fn submit_sentence(
-    language: &str,
-    text: &str,
-) -> Result<(), String> {
+fn submit_sentence(language: &str, text: &str) -> Result<(), String> {
     let base_dir = tauri::api::path::public_dir().unwrap();
 
     let row = Row {
@@ -92,10 +93,13 @@ fn submit_sentence(
     };
 
     let tmp_file_path = base_dir.join("tmp.csv");
-    
+
     let rows = count_csv_rows(&tmp_file_path);
     write_sentence(&row, &tmp_file_path, rows == 0);
-    let sentences_per_csv = env::var("SENTENCES_PER_CSV").unwrap_or(format!("{}", SENTENCES_PER_CSV)).parse::<usize>().unwrap();
+    let sentences_per_csv = env::var("SENTENCES_PER_CSV")
+        .unwrap_or(format!("{}", SENTENCES_PER_CSV))
+        .parse::<usize>()
+        .unwrap();
 
     log::info!("{}/{} rows in tmp.csv", rows + 1, sentences_per_csv);
 
@@ -110,9 +114,44 @@ fn submit_sentence(
 
 #[tauri::command]
 fn get_env_var(key: &str) -> String {
-    let res =    env::var(key).unwrap_or("".to_string());
+    let res = env::var(key).unwrap_or("".to_string());
     log::info!("Getting env var {}", res);
     res
+}
+
+fn setup_osc_receiver(app: &mut tauri::App) {
+    thread::spawn(|| {
+        // Bind the UDP socket to listen on port 7000
+        let socket = UdpSocket::bind("127.0.0.1:7000").unwrap();
+
+        let mut buf = [0u8; rosc::decoder::MTU];
+
+        loop {
+            match socket.recv_from(&mut buf) {
+                Ok((size, addr)) => {
+                    println!("Received packet with size {} from: {}", size, addr);
+                    let (_, msg) = rosc::decoder::decode_udp(&buf[..size]).unwrap();
+                    handle_packet(msg);
+                }
+                Err(e) => {
+                    println!("Error receiving from socket: {}", e);
+                    break;
+                }
+            }
+        }
+    });
+}
+
+fn handle_packet(packet: OscPacket) {
+    match packet {
+        OscPacket::Message(msg) => {
+            println!("OSC address: {}", msg.addr);
+            println!("OSC arguments: {:?}", msg.args);
+        }
+        OscPacket::Bundle(bundle) => {
+            println!("OSC Bundle: {:?}", bundle);
+        }
+    }
 }
 
 fn main() {
@@ -122,6 +161,21 @@ fn main() {
                 .targets([LogTarget::LogDir, LogTarget::Stdout, LogTarget::Webview])
                 .build(),
         )
+        .plugin(tauri_plugin_store::Builder::default().build())
+        .setup(|app| {
+            tauri_plugin_store::StoreBuilder::new(
+                app.handle(),
+                app.path_resolver()
+                    .app_config_dir()
+                    .unwrap()
+                    .join(".settings"),
+            )
+            .build();
+
+            setup_osc_receiver(app);
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![submit_sentence, get_env_var])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
